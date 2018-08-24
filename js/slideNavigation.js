0cd90760dba7e9d31ed4b4b2369530731e592dae
eoneu17 <41932095+eoneu17@users.noreply.github.com>
2018-08-24 16:18:04 +0900

    overlay

--- js/slideNavigation.js
+++ js/slideNavigation.js
@@ -1,6 +1,7 @@
-function openNav(){
-  document.getElementById('mySlidenav').style.width='30%';
+function openNav() {
+  document.getElementById('mySlidenav').style.width = '30%';
 }
-function closeNav(){
-  document.getElementById('mySlidenav').style.width="0"
+
+function closeNav() {
+  document.getElementById('mySlidenav').style.width = "0"
 }
